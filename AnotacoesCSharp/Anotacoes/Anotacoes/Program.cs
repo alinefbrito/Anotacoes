24bb881c1e60464b77ab4b18876f02922cd468c3
alinefbrito <alinefbrito@gmail.com>
2020-04-03 16:26:05 -0300

    Adicionar projeto C#

---
+++ AnotacoesCSharp/Anotacoes/Anotacoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anotacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! :D");
+        }
+    }
+}
